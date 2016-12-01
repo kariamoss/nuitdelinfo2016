a692d520622eb6997cbf420179f558e82e905d0d
Josue Collombier <josue.collombier@etu.unice.fr>
2016-12-01 23:34:32 +0100

    dohbqemrgbqerlkvjbqd

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:9090/nuitdelinfo2016';
+$config['base_url'] = 'http://localhost/nuitdelinfo2016';
 
 /*
 |--------------------------------------------------------------------------
