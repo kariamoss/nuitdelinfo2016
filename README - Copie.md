26c67b1dfab6921e8665fd28d70d76c20d009a95
Kariamoss <jehanmilleret@gmail.com>
2016-12-02 03:20:01 +0100

    Supprimed codeigniter, composer, twig

--- README - Copie.md
+++
@@ -1,1 +0,0 @@
-# nuitdelinfo2016
---
+++ js/request.js
@@ -0,0 +1,20 @@
+function request(action){
+
+  var xhr = new XMLHttpRequest()
+  xhr.open('GET', '/serv.php?req='+action)
+
+  //On envoie les données d’un formulaire
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState == 4) {
+      if (xhr.status == 200) {
+        document.getElementById('posts').innerHTML = xhr.responseText
+    }
+  }
+}
+
+xhr.send()
+
+}
+
+
